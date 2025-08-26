8f7e9d4ef1707068e15649e88cc079ac634793b0
Emanuele De Santis <trunk96@gmail.com>
2025-08-26 13:13:36 +0200

    removed unused table

--- spotifyfamily/models.py
+++ spotifyfamily/models.py
@@ -13,14 +13,10 @@ class Subscription(models.Model):
     
 class User(AbstractUser):
     email = models.EmailField('email address', unique=True)
+    subscriptions = models.ManyToManyField(Subscription, through="SubscriptionDetails")
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-class UserSubscription(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    subscriptions = models.ManyToManyField(Subscription, through="SubscriptionDetails")
-    def __str__(self):
-        return self.user.username
 
 class SubscriptionDetails(models.Model):
     last_payment_date = models.DateField()
