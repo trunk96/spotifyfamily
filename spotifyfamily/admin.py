44ad6bbe20727e472f97f9a3e0e4bd6fa93f1007
Emanuele De Santis <trunk96@gmail.com>
2025-08-26 14:58:54 +0200

    now the admin can create subscriptions

--- spotifyfamily/admin.py
+++ spotifyfamily/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Subscription, SubscriptionDetails
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+admin.site.register(Subscription)
+admin.site.register(SubscriptionDetails)
