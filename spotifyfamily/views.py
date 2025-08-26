58067a7bd2524f31eb3efc51a9cd15c7671cb0a3
Emanuele De Santis <trunk96@gmail.com>
2025-08-26 15:11:35 +0200

    changed template path

--- spotifyfamily/views.py
+++ spotifyfamily/views.py
@@ -6,6 +6,6 @@ from .models import Subscription
 # Create your views here.
 def index(request):
     subscriptions = Subscription.objects.order_by("-start_date")
-    template = loader.get_template("polls/index.html")
+    template = loader.get_template("index.html")
     context = {"subscriptions": subscriptions}
     return HttpResponse(template.render(context, request))
