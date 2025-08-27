62cbdb5a994d8b06829f4f1ecf984b003819f999
Emanuele De Santis <trunk96@gmail.com>
2025-08-27 10:38:43 +0200

    fixed missing related name for backward reference of User from Subscription

--- spotifyfamily/models.py
+++ spotifyfamily/models.py
@@ -6,7 +6,7 @@ class Subscription(models.Model):
     start_date = models.DateField()
     name = models.CharField(max_length=100)
     users = models.ManyToManyField("User", through="SubscriptionDetail")
-    admin_user = models.ForeignKey("User", on_delete=models.CASCADE)
+    admin_user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="admin_subscriptions")
     renew_period = models.IntegerField(default=1)  # in months
     def __str__(self):
         return f"{self.name} - from {self.start_date}"
