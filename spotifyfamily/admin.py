99c0ab846387ffc4c43e361338d5a789b86dbc6a
Emanuele De Santis <trunk96@gmail.com>
2025-08-27 15:23:57 +0200

    added subscription price to admin view

--- spotifyfamily/admin.py
+++ spotifyfamily/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Subscription, SubscriptionDetail
+from .models import User, Subscription, SubscriptionDetail, SubscriptionPrice
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(Subscription)
 admin.site.register(SubscriptionDetail)
+admin.site.register(SubscriptionPrice)
