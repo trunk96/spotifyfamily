352376f481e7586d71f3558def3d295183f75d38
Emanuele De Santis <trunk96@gmail.com>
2025-08-28 09:18:33 +0200

    added authentication from django auth

--- spotifyfamily/urls.py
+++ spotifyfamily/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
+
 from . import views
+from django.urls import include
 
 urlpatterns = [
     #path("", views.index, name="index"),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),  # new
-    path("login/", TemplateView.as_view(template_name="registration/login.html"), name="login"),
+    path("", include("django.contrib.auth.urls")),
 ]
