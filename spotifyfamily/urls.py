219d7bcae80c0c95b2c61be4665bf57df04c48e7
Emanuele De Santis <trunk96@gmail.com>
2025-08-28 09:15:02 +0200

    added login page

--- spotifyfamily/urls.py
+++ spotifyfamily/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    #path("", views.index, name="index"),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),  # new
+    path("login/", TemplateView.as_view(template_name="registration/login.html"), name="login"),
 ]
