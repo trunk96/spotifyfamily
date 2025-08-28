d9f91ea662a5606ac09a55019bec2b273d9a642b
Emanuele De Santis <trunk96@gmail.com>
2025-08-28 11:16:09 +0200

    changed login form

--- spotifyfamily/views.py
+++ spotifyfamily/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
 from .models import Subscription
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def index(request):
@@ -9,3 +11,22 @@ def index(request):
     template = loader.get_template("index.html")
     context = {"subscriptions": subscriptions}
     return HttpResponse(template.render(context, request))
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                return render(request, "registration/login.html", {"form": form, "message": "Invalid username or password."})
+        else:
+            return render(request, "registration/login.html", {"form": form, "message": "Invalid username or password."})
+    else:
+        form = AuthenticationForm()
+        return render(request, "registration/login.html", {"form": form})
