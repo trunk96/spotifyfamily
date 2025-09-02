9e79f970497b59eb0e066816fd1c5c813547fc70
Emanuele De Santis <trunk96@gmail.com>
2025-09-02 09:07:07 +0200

    added logout

--- spotifyfamily/views.py
+++ spotifyfamily/views.py
@@ -3,7 +3,9 @@ from django.template import loader
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
 from .models import Subscription
+from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout
 
 # Create your views here.
 def index(request):
@@ -13,7 +15,7 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
-def login(request):
+def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
@@ -24,9 +26,15 @@ def login(request):
                 login(request, user)
                 return redirect("home")
             else:
-                return render(request, "registration/login.html", {"form": form, "messages": ["Invalid username or password."]})
+                messages.error(request, 'Invalid Username or Password')
+                return render(request, "registration/login.html", {"form": form})
         else:
-            return render(request, "registration/login.html", {"form": form, "messages": ["Invalid username or password."]})
+            messages.error(request, 'Invalid CSRF Token')
+            return render(request, "registration/login.html", {"form": form})
     else:
         form = AuthenticationForm()
         return render(request, "registration/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
